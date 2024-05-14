f0f8c3077e3518676011f0f237d6652738f6aa30
jsola123 <65975649+jsola123@users.noreply.github.com>
2024-05-14 14:01:16 -0400

    Adding AR Indoor Navigation Project

---
+++ Indoor Navigation/Assets/Scripts/Target.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+   [System.Serializable]
+
+public class Target
+{
+    public string Name;
+    public GameObject PositionObject;
+}
